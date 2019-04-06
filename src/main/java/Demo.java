cdc16e00f969f84d27faabfae113e4b0c8e2465a
wangwenjie1997 <2589856016@qq.com>
2019-04-06 15:23:59 +0800

    日志输出学习

---
+++ src/main/java/Demo.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+
+public class Demo {
+
+    private static final Logger logger=Logger.getLogger(Demo.class);
+
+    public static void main(String[] args) {
+
+        logger.info("日志输出");
+    }
+
+}
